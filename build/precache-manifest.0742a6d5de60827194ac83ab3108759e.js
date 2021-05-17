c31befa477bc90634ff54d251d46c2b733d010f9
SnowMoon272 <CastielAltair0027@outlook.com>
2021-05-17 00:23:18 -0500

    build creado

---
+++ build/precache-manifest.0742a6d5de60827194ac83ab3108759e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "12f68bdb33064f93ad92",
+    "url": "/static/css/main.541e2394.chunk.css"
+  },
+  {
+    "revision": "12f68bdb33064f93ad92",
+    "url": "/static/js/main.12f68bdb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7241f2c9401c3cf8c716",
+    "url": "/static/css/2.79ce03f0.chunk.css"
+  },
+  {
+    "revision": "7241f2c9401c3cf8c716",
+    "url": "/static/js/2.7241f2c9.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "3a09ec40228d5374dbf79ad8bd2731e8",
+    "url": "/static/media/Taken-bro.3a09ec40.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "7a2066a38fe8e34db8c1c80bef85981e",
+    "url": "/index.html"
+  }
+];
